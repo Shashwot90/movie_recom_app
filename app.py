27022596b027a90559effb9cf27197f62aa76665
unknown <shashwotpradhan@gmail.com>
2024-10-31 13:29:22 +0545

    getting response in json

--- app.py
+++ app.py
@@ -5,4 +5,5 @@ import requests
 
 def fetch_poster(movie_id):
     response = requests.get('https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US'.format(movie_id))
+    data = response.json()
     
