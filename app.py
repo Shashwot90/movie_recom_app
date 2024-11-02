97785620338395c7d1ff8c7bce03aa3ea3654019
unknown <shashwotpradhan@gmail.com>
2024-11-02 13:48:01 +0545

    recommend function

--- app.py
+++ app.py
@@ -7,3 +7,5 @@ def fetch_poster(movie_id):
     response = requests.get('https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US'.format(movie_id))
     data = response.json()
     return "https://image.tmdb.org/t/p/w500/" + data['poster_path']
+def recommend(movie):
+    
